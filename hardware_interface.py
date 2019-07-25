3a0617445cb7ac9827360b51fd864735cd356bf0
David Croft <ac0745@coventry.ac.uk>
2019-07-25 10:14:08 +0100

    rest calls

--- hardware_interface.py
+++ hardware_interface.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from tornado import websocket, web, ioloop, gen
+import tornado
 import json
 import time, random
 import csv, struct
@@ -10,6 +11,7 @@ class StreamHandler( websocket.WebSocketHandler ):
 	cl = {}
 
 	def open( self, *args ):
+		print( "open connection" )
 		uri = self.request.uri
 		if uri not in __class__.cl:
 			__class__.cl[uri] = set()
@@ -25,7 +27,10 @@ class StreamHandler( websocket.WebSocketHandler ):
 		if path not in __class__.cl: return 0
 
 		for client in __class__.cl[path]:
-			client.write_message( message )
+			try:
+				client.write_message( message )
+			except:
+				pass
 
 		return len(__class__.cl[path])
 
@@ -33,7 +38,6 @@ class StreamHandler( websocket.WebSocketHandler ):
 	def send_message( data, path ):
 		""" Send message as JSON """
 		j = json.dumps( data )
-
 		return __class__.__send( j, path )
 
 	@staticmethod
@@ -44,12 +48,85 @@ class StreamHandler( websocket.WebSocketHandler ):
 
 		return __class__.__send( b, path )
 
+class RepHandler( web.RequestHandler ):
+	reps = 0
+	repstate = "low"
+
+	def get(self):
+		self.write( {"value":__class__.reps} )
+
+	def put(self):
+		try: 
+			value = int( json.loads(self.request.body)["value"] )
+			__class__.reps = value
+		except:
+			self.set_status(400)
+		
+		self.get()		
+
+	@staticmethod
+	def poll( position ):
+		# count the reps
+		if __class__.repstate == "low" and position > 15000:
+			__class__.repstate = "high"
+			__class__.reps += 1
+		elif __class__.repstate == "high" and position < 10000:
+			__class__.repstate = "low"
+
+class ProgrammeHandler( web.RequestHandler ):
+	__options = ("aaa", "bbb", "ccc")
+	__value = "aaa"
+
+	def get(self):
+		self.write( { "options": __class__.__options, \
+					  "value": __class__.__value } )
+
+	def put(self):
+		try:
+			value = json.loads(self.request.body)["value"]
+			if value not in __class__.__options:
+				raise
+			__class__.__value = value
+		except:
+			self.set_status(400)
+
+		self.get()
+
+class BasicLoadHandler( web.RequestHandler ):
+	__value = 0
+
+	def get(self):
+		self.write( {"value": __class__.__value} )
+
+class VariableLoadHandler( web.RequestHandler ):
+	__value = 0
+
+	def get(self):
+		self.write( {"value": __class__.__value} )
+
+	def put(self):
+		try:
+			value = json.loads(self.request.body)["value"]
+			if value not in __class__.__options:
+				raise
+			__class__.__value = value
+		except:
+			self.set_status(400)
+
+		self.get()
+
+class GainHandler( web.RequestHandler ):
+	__value = 0
+
+	def get(self):
+		self.write( {"value": __class__.__value} )
 
 @gen.coroutine
 def main( data ):
 	""" Websocket publish loop 
 		Would be reading from CAN and serial normally """
 	row = 0
+	repstate = "low"
 	prev = datetime.datetime.now()
 	while True:
 		now = datetime.datetime.now()
@@ -60,6 +137,8 @@ def main( data ):
 		values = data[row]
 		can = filter_values( values, "can" )
 		serial = filter_values( values, "serial" )
+
+		RepHandler.poll( serial["position"] )
 
 		StreamHandler.send_message( can, "/can/json" )
 		StreamHandler.send_message( serial, "/serial/json" )
@@ -95,7 +174,12 @@ if __name__ == '__main__':
 	data = load_example_data( "data.csv" )
 
 	print( "Running" )
-	app = web.Application( ((r"/(can|serial)/(json|bin)", StreamHandler),), debug=True )
+	app = web.Application( ( (r"/(can|serial)/(json|bin)", StreamHandler), \
+							 (r"/reps", RepHandler), \
+							 (r"/programme", ProgrammeHandler), \
+							 (r"/load/basic", BasicLoadHandler), \
+							 (r"/load/variable", VariableLoadHandler), \
+							 (r"/gain", GainHandler) ), debug=True )
 	app.listen(5001)
 
 	loop = ioloop.IOLoop.instance()
